f2769d0bc9685b3e32493075a2fa5a12d0d19677
azri <muhd.azri@fiction-labs.com>
2017-10-17 12:01:35 +0800

    fresh project

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by azri on 17/10/2017.
+//  Copyright © 2017 Muhammad Azri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
